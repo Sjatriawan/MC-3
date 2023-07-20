2a8ff4376cfb548f26b68f197a7b5316e2d4012b
Sjatriawan <aryadendi@gmail.com>
2023-07-20 10:06:50 +0700

    add tab bar trip and wishlist

---
+++ Enviroamer/Screen/Wishlist/WishlistScreen.swift
@@ -0,0 +1,20 @@
+//
+//  WishlistScreen.swift
+//  Enviroamer
+//
+//  Created by M Yogi Satriawan on 20/07/23.
+//
+
+import SwiftUI
+
+struct WishlistScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WishlistScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        WishlistScreen()
+    }
+}
