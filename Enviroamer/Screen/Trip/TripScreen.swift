496b13245559d3c452ae9a34ef8479231cc8b7bd
Sjatriawan <aryadendi@gmail.com>
2023-07-20 20:13:47 +0700

    Konek to map

--- Enviroamer/Screen/Trip/TripScreen.swift
+++ Enviroamer/Screen/Trip/TripScreen.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct TripScreen: View {
     var body: some View {
-        Text("Trip Screen")
+        ZStack{
+            MapScreen()
+            
+            
+
+        }.ignoresSafeArea()
     }
 }
 
